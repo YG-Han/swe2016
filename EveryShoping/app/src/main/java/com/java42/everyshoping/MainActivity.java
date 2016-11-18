e133b11c0f710dd697137fa30774ccc10bb0dfde
YeongGuk Han <music2809@gmail.com>
2016-11-18 21:07:55 +0900

    안드로이드 개발
    안드로이드 어플(쇼핑몰 웹 뷰) 개발

---
+++ EveryShoping/app/src/main/java/com/java42/everyshoping/MainActivity.java
@@ -0,0 +1,21 @@
+package com.java42.everyshoping;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://swe2016.cafe24.com/wp");
+
+    }
+
+}
